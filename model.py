aba63966eff6491f788804400447123613c0f868
VladaTrus <47668051+VladaTrus@users.noreply.github.com>
2020-11-09 16:19:50 +0300

    Create model.py

---
+++ model.py
@@ -0,0 +1,61 @@
+import torch
+import torch.nn as nn
+
+class Attention(nn.Module):
+    def __init__(self, input_size, hidden_size):
+        super().__init__()
+        self.l1 = nn.Linear(input_size, hidden_size, bias=True)
+        self.l2 = nn.Linear(hidden_size, 1, bias=False)
+        self.tanh = nn.Tanh()
+
+    def forward(self, x):
+        output = self.l1(x)
+        output = self.tanh(output)
+        output = self.l2(output)
+        output = torch.softmax(output, dim=1)
+        output = (x * output).sum(dim=1)
+        return output
+        
+class KWS_model(nn.Module):
+    def __init__(self, 
+                 n_mels = 40,
+                 cnn_channels = 16, 
+                 cnn_kernel_size = 51,
+                 gru_hidden_size = 64, 
+                 attention_hidden_size = 64):
+      
+        super().__init__()
+        self.cnn = nn.Conv1d(n_mels, cnn_channels, kernel_size=cnn_kernel_size, 
+                             padding=cnn_kernel_size // 2)
+        self.relu = nn.ReLU()
+        self.rnn = nn.GRU(input_size=cnn_channels, hidden_size=gru_hidden_size, 
+                          bidirectional=True, batch_first=True)
+        self.attention = Attention(gru_hidden_size * 2, attention_hidden_size)
+        self.linear = nn.Linear(gru_hidden_size * 2, 2, bias=False)
+        self.softmax = nn.LogSoftmax(dim=1)
+
+    def forward(self, x):
+        output = self.cnn(x)
+        output = self.relu(output).permute(0, 2, 1)
+        output, hidden = self.rnn(output)
+        output = self.attention(output)
+        output = self.linear(output)
+        output = self.softmax(output)
+        return output
+    
+
+    def inference(self, x, window_size = 100):
+        if window_size > x.shape[2]:
+            window_size = x.shape[2]
+        probabilities = []
+        for i in range(window_size, x.shape[2] + 1, 40):
+            win = x[:, :, i - window_size:i]
+            win = self.cnn(win)
+            win = self.relu(win)
+            win = win.permute(0, 2, 1)
+            win, _ = self.rnn(win)
+            win = self.attention(win)
+            win = self.linear(win)
+            pr = torch.softmax(win, dim=1)
+            probabilities.append(pr[0][1].item())
+        return probabilities
